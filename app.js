acfb7d65c0ad1c1666e3745e6b1deb88a96c3dd3
unknown <pampambriceno.dev@gmail.com>
2025-01-30 19:15:38 -0500

    feat: Juego completo

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
-let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un numero por favor:");
+let nombre = prompt('Por favor, ingresa tu nombre')
+alert(`¡Hola ${nombre}! Te damos la bienvenida a Guess the Number`)
+let numeroMaximoPosible = prompt('Tú decides la dificultad! Ingresa el número (rango) máximo a adivinar. Ejemplo: 10, 20, 50, etc.');
 
-console.log(numeroUsuario);
+let numeroSecreto = Math.floor(Math.random()*numeroMaximoPosible)+1;
+let numeroUsuario = 0;
+let intentos = 1;
+let maximosIntentos = 5;
 
-if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el numero');
+while (numeroUsuario != numeroSecreto) {
+    numeroUsuario = parseInt(prompt(`Ingresa un número entre 1 y ${numeroMaximoPosible}`));
+
+    console.log(numeroUsuario);
+    
+    if (numeroUsuario == numeroSecreto) {
+        alert(`¡Acertaste! El número era ${numeroSecreto}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}`);
+    } else {
+        if (numeroUsuario > numeroSecreto) {
+            alert('El número secreto es menor')
+        } else {
+            alert('El número secreto es mayor')
+        }
+        intentos++;
+
+        if (intentos > maximosIntentos){
+            alert(`Llegaste al número máximo de ${maximosIntentos} intentos`);
+            break;
+        }
+    }
 }
